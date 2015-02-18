d61c8dc5fc237bc4de43039d06e1b48dc7d44305
Andrew Godwin <andrew.godwin86@gmail.com>
2015-02-18 13:36:13 +1100

    Started nexus db install function

--- includes/class-plugin-name-activator.php
+++ includes/class-plugin-name-activator.php
@@ -30,7 +30,25 @@ class Plugin_Name_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		function nexus_db_install() {
 
+			global $wpdb;
+
+			$table_name = $wpdb->prefix . "nexusSyncDates";
+
+			$charset_collate = $wpdb->get_charset_collate();
+
+			$sql = "CREATE TABLE $table_name (
+			  id mediumint(9) NOT NULL AUTO_INCREMENT,
+			  entityName tinytext NOT NULL,
+			  syncLastRun datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			  text text NOT NULL,
+			  url varchar(55) DEFAULT '' NOT NULL,
+			  UNIQUE KEY id (id)
+			) $charset_collate;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+		}
 	}
-
 }
