c4a827e42683fc5a9db60a8b90e8e73e5734f407
Andrew Godwin <andrew.godwin86@gmail.com>
2015-02-18 13:37:38 +1100

    Consistent whitespace formatting

--- includes/NexusApiClient.php
+++ includes/NexusApiClient.php
@@ -8,20 +8,20 @@ class NexusAPIClient{
 	private $apiClient;
 	private $username;
 	private $password;
-	
+
 	public function __construct ($args = null){
-		//parent::__construct($args);		
+		//parent::__construct($args);
 		$this->client = new Client(KA_API_BASEURL.'/api/{version}', array(
-		    'version' => KA_API_VERSION,
+			'version' => KA_API_VERSION,
 				'request.options' => array(
-				        //'headers' => array('Accept' => 'text/json'),
-				        //'query'   => array('testing' => '123'),
-				        'auth'    => array(KA_API_CLIENT_ID, KA_API_CLIENT_KEY, 'Basic'),
-				        //'proxy'   => 'tcp://localhost:80'
-				    )
+					//'headers' => array('Accept' => 'text/json'),
+					//'query'   => array('testing' => '123'),
+					'auth' => array(KA_API_CLIENT_ID, KA_API_CLIENT_KEY, 'Basic'),
+					//'proxy'   => 'tcp://localhost:80'
+				)
 		));
 	}
-	
+
 	public function GetEntiteesUpdatedSince($entityType,$lastModDate)
 	{
 		date_default_timezone_set('America/Los_Angeles');
@@ -33,62 +33,62 @@ class NexusAPIClient{
 		$response = $request->send();
 
 		$data = $response->json();
-		
-		return $data['results'];		
+
+		return $data['results'];
 	}
 
 	public function GetEntityByID($entityType, $entityID)
 	{
 		$request = $this->client->get($entityType.'/'.$entityID);
-		
+
 		$response = $request->send();
 
 		$data = $response->json();
-		
+
 		return $data['results'][0];
 	}
 
 	public function GetRelatedEntityByEntityID($relatedEntity, $entityType, $entityID)
 	{
 		$request = $this->client->get($entityType.'/'.$entityID.'/'.$relatedEntity);
-		
+
 		$response = $request->send();
 
 		$data = $response->json();
-		
-		return $data['results'];		
+
+		return $data['results'];
 	}
 
 	public function GetProjectMediaByID($projectID)
 	{
 		$request = $this->client->get('projects/'.$projectID.'/media');
-		
+
 		$response = $request->send();
 
 		$data = $response->json();
-		
+
 		return $data['results'];
 	}
 
 	public function GetProjectDescriptionsByID($projectID)
 	{
 		$request = $this->client->get('projects/'.$projectID.'/descriptions');
-		
+
 		$response = $request->send();
 
 		$data = $response->json();
-		
+
 		return $data['results'];
 	}
 
 	public function GetProjectsBySector($sector)
 	{
 		$request = $this->client->get('projects?projectType='.$sector);
-		
+
 		$response = $request->send();
 
 		$data = $response->json();
-		
+
 		return $data['results'];
 	}
 
